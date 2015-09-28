fce8a558e49260669b9c9f9d41dcac844876ea30
Dan Tong <tongvtdan@outlook.com>
2015-09-28 07:47:42 +0700

    Add Map class to manage all features related to map and location

---
+++ KiVi/Map.swift
@@ -0,0 +1,22 @@
+//
+//  Map.swift
+//  KiVi
+//
+//  Created by Dan Tong on 9/28/15.
+//  Copyright © 2015 Dan Tong. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Map: NSObject, MKMapViewDelegate {
+  
+  func getCurrentLocation() {
+    
+  }
+  
+  func searchLocationWithAddress(address: String) {
+    
+  }
+
+}
