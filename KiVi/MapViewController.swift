b8430cb69dc30f168104fc5c7b77ec8ec4342fb9
Dan Tong <tongvtdan@outlook.com>
2015-09-28 12:14:04 +0700

    Add job on map: done

--- KiVi/MapViewController.swift
+++ KiVi/MapViewController.swift
@@ -15,7 +15,19 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
   
   @IBOutlet var jobMap: MKMapView!
   
-  var locationManager: CLLocationManager!
+  var selectedJob: PFObject?
+  var jobsList: [PFObject]? = [PFObject]()
+  
+  var localSearchRequest:MKLocalSearchRequest!
+  var localSearch:MKLocalSearch!
+  var localSearchResponse:MKLocalSearchResponse!
+  var pointAnnotation:MKPointAnnotation!
+  var pinAnnotationView:MKPinAnnotationView!
+  
+  var locationManager = CLLocationManager()
+  var userAnnotation = MKPointAnnotation()
+  
+  var timer: NSTimer = NSTimer()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -34,10 +46,51 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
   override func viewDidAppear(animated: Bool) {
     
     locationManager.startUpdatingLocation()
+    timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "fetchJobsInformation", userInfo: nil, repeats: true)
   }
   
   override func viewDidDisappear(animated: Bool) {
     locationManager.stopUpdatingLocation()
+  }
+  
+  // MARK: Map View Delegate protocol
+  
+  func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+    if !(annotation is  MKPointAnnotation) {
+      return nil
+    }
+    
+    // Resize the image selected
+    let resizeRenderImageView = UIImageView(frame: CGRectMake(0, 0, 45, 45))
+    resizeRenderImageView.layer.borderColor = UIColor.whiteColor().CGColor
+    resizeRenderImageView.layer.borderWidth = 3.0
+    resizeRenderImageView.contentMode = UIViewContentMode.ScaleAspectFill
+    resizeRenderImageView.image = UIImage(named: "defaultImage")
+    
+    UIGraphicsBeginImageContext(resizeRenderImageView.frame.size)
+    resizeRenderImageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+    let thumbnail = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    
+    let reuseID = "myAnnotationView"
+    var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseID)
+    if (annotationView == nil) {
+      // Must use MKAnnotationView instead of MKPointAnnotationView if we want to use image for pin annotation
+      annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+      annotationView!.canShowCallout = true
+      annotationView!.image = thumbnail
+      // Left Image annotation
+      annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:80))
+      let imageView = annotationView!.leftCalloutAccessoryView as! UIImageView
+      imageView.image = UIImage(named: "defaultImage")
+      // Right button annotation
+      annotationView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIButton
+    }
+    else {
+      annotationView!.annotation = annotation
+    }
+    
+    return annotationView
   }
   
   // MARK: Location Manager Delegate
@@ -52,6 +105,59 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
   }
   
+  func fetchJobsInformation() {
+    jobsList = ParseInterface.sharedInstance.getJobsInformation()
+    self.updateJobsMap()
+    print("Getting joblist")
+    
+  }
+  
+  func updateJobsMap() {
+    if jobsList?.count > 0 {
+      for var i = 0; i < jobsList?.count; i++ {
+        selectedJob = jobsList![i]
+        pinJobOnMap(selectedJob)
+      }
+      timer.invalidate()
+    }
+    
+  }
+  
+  // View job on Map
+  let regionRadius: CLLocationDistance = 20000 // 20 km
+  
+  func pinJobOnMap(jobToPin: PFObject?) {
+    localSearchRequest = MKLocalSearchRequest()
+    localSearchRequest.naturalLanguageQuery = jobToPin!["contactAddress"] as? String
+    
+    localSearch = MKLocalSearch(request: localSearchRequest)
+    localSearch.startWithCompletionHandler { (localSearchResponse, error) -> Void in
+      
+      if localSearchResponse == nil{
+        let alert = UIAlertView(title: nil, message: "Place not found", delegate: self, cancelButtonTitle: "Try again")
+        alert.show()
+        return
+      }
+      //3
+      self.pointAnnotation = MKPointAnnotation()
+      self.pointAnnotation.title    = jobToPin!["jobTitle"] as? String
+      self.pointAnnotation.subtitle = jobToPin!["contactAddress"] as? String
+      
+      self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: localSearchResponse!.boundingRegion.center.latitude, longitude:     localSearchResponse!.boundingRegion.center.longitude)
+      
+      
+      self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+      self.jobMap.centerCoordinate = self.pointAnnotation.coordinate
+      self.jobMap.addAnnotation(self.pinAnnotationView.annotation!)
+      
+      let coordinateRegion = MKCoordinateRegionMakeWithDistance(self.pointAnnotation.coordinate, self.regionRadius, self.regionRadius)
+      self.jobMap.setRegion(coordinateRegion, animated: true)
+      
+      
+    }
+    
+  }
+
   
   func updateUserCurrentLocation(userLocation: CLLocation) {
     let latitude = userLocation.coordinate.latitude
@@ -95,8 +201,19 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
           }
           let title = "\(subThoroughfare), \(thoroughfare)"
           let addr = "\(pm.subLocality!), \(pm.subAdministrativeArea!), \(pm.administrativeArea!) \(pm.country!)"
-          let annotation = JobAnnotation(title: title, address: addr, coordinate: CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude))
-          self.jobMap.addAnnotation(annotation)
+          
+          
+          self.pointAnnotation = MKPointAnnotation()
+          self.pointAnnotation.title    = title
+          self.pointAnnotation.subtitle = addr
+          
+          self.pointAnnotation.coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+          
+          self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: nil)
+          self.jobMap.centerCoordinate = self.pointAnnotation.coordinate
+          self.jobMap.addAnnotation(self.pinAnnotationView.annotation!)
+ 
+          
       }
     }
   }
